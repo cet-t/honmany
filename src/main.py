f82a96733fd9f7b7c1438dc7b186eca68b40c9e3
cet <cets021@gmail.com>
2024-01-04 00:17:07 +0900

    a

--- src/main.py
+++ src/main.py
@@ -2,18 +2,18 @@ import discord
 from discord import app_commands
 from random import randint
 import numpy as np
-import json
 
 from trrne.lottery import Lottery
 from trrne.m import M
 from for4 import *
 from emb import *
 
+
 bot = discord.Client(intents=discord.Intents.default())
 tree = app_commands.CommandTree(bot)
 
 
-def filepath(id: int):
+def nickname_file_path(id: int):
     return f'./nicknames/{id}.txt'
 
 
@@ -28,30 +28,39 @@ async def cc(action: discord.Interaction):
 @tree.command(name='ほんめにー', description='あいさつ')
 @app_commands.guild_only()
 async def honmany(action: discord.Interaction):
-    with open(filepath(action.user.id), 'r', encoding=ENCODE) as f:
-        names = f.read().split(' ')
-    print(str.join(',', names))
-    await action.response.send_message(fr'{names[randint(0, len(names)-1)]}サン、コンニチハ！')
+    try:
+        with open(nickname_file_path(action.user.id), 'r', encoding=ENCODE) as f:
+            names = f.read().split(' ')
+        print(str.join(',', names))
+        await action.response.send_message(f'{names[randint(0, len(names)-1)]}サン、コンニチハ！')
+    except FileNotFoundError as e:
+        raise e
 
 
-@tree.command(name='名前登録', description='名前を追加する')
-@app_commands.describe(target='追加したいメンバー', addition='追加したい名前')
+@tree.command(name='名前追加', description='新しい呼び名を追加')
+@app_commands.describe(target='対象のメンバー', addition='追加したい名前')
 async def add_name(action: discord.Interaction, target: discord.Member, addition: str):
     # http://www.not-enough.org/abe/manual/api-aa09/fileio.html
-    with open(filepath(target.id), 'a', encoding=ENCODE) as f:
-        f.write(f' {addition}')
-    await action.response.send_message('success', ephemeral=True)
+    try:
+        with open(nickname_file_path(target.id), 'a', encoding=ENCODE) as f:
+            f.write(f' {addition}')
+            await action.response.send_message('success', ephemeral=True)
+    except:
+        await action.response.send_message('rejected', ephemeral=True)
 
 
 # TODO 動作確認
 @tree.command(name='名前削除', description='登録された名前を削除する')
-@app_commands.describe(target='メンバー', delete='削除したい名前')
+@app_commands.describe(target='対象のメンバー', delete='削除したい名前')
 async def delete_name(action: discord.Interaction, target: discord.Member, delete: str):
-    with open(filepath(target.id), 'w+', encoding=ENCODE) as f:
-        names = f.read().split(' ')
-        names.remove(delete)
-        f.write(str.join(' ', names))
-    await action.response.send_message('success', ephemeral=True)
+    try:
+        with open(nickname_file_path(target.id), 'w+', encoding=ENCODE) as f:
+            names = f.read().split(' ')
+            names.remove(delete)
+            f.write(str.join(' ', names))
+            await action.response.send_message('success', ephemeral=True)
+    except:
+        await action.response.send_message('rejected', ephemeral=True)
 
 
 @tree.command(name='くじ10口', description='道徳46点の方向け')
@@ -71,7 +80,8 @@ async def lotn(action: discord.Interaction, count: int):
     count = np.abs(count)
     for i in range(count):
         index = Lottery.bst(kuji.weights())
-        dst += f'{i+1}: {kuji.subjects()[index]}({M.floor(kuji.weights()[index]/kuji.total_weight()*100, 2)}%)' + \
+        per = M.floor(kuji.weights()[index]/kuji.total_weight()*100, 2)
+        dst += f'{i+1}: {kuji.subjects()[index]}({per}%)' + \
             ('\n' if i < count else '')
     await action.response.send_message(dst if dst.__len__() < 2000 else dst[:2000-1])
 
